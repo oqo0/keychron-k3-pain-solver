f09819a45033d19178ec615bec6fe9df7b157128
oqo0 <51176627+oqo0@users.noreply.github.com>
2022-03-26 10:52:49 +0300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 
-# Fn + F-keys fix
+# fn + f keys fix
 echo "options hid_apple fnmode=2" | sudo tee /etc/modprobe.d/hid_apple.conf
 sudo dracut --regenerate-all
 
 
-# Bluetooth fix
+# bluetooth fix
 CONFIG_FILE='/etc/bluetooth/main.conf'
 sed -c -i "s/\('FastConnectable' *= *\).*/\1'true'/" $CONFIG_FILE
 sed -c -i "s/\('ReconnectAttempts' *= *\).*/\1'15'/" $CONFIG_FILE
 sed -c -i "s/\('ReconnectIntervals' *= *\).*/\1'1, 2, 3, 5'/" $CONFIG_FILE
 
 
-# Bluetooth after waking up from sleep fix
+# bluetooth after waking up from sleep fix
 sudo tee /lib/systemd/system-sleep/bt << EOT
 #!/bin/sh
 case $1 in
